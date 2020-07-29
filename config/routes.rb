b19113d91cb1684f1e55560e3fe598c304b9d779
Muhammd Umar <umar.omtomo@gmail.com>
2020-07-29 12:36:51 +0500

    added sitemap file and robot.txt

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 	# AMP
+  get '/sitemap' => 'sitemap#index',defaults: {format: "xml"}
   get '/amp' => 'amp#index'
   get '/amp/sim-deal-only' => 'amp#sim_deal_only'
   post '/validate_data' => 'form_validation#valid_data'
