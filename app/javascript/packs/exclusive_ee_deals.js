d81eb0e8083dbd3d7d2c0f2a6f1d1b60ae81cb8c
Muhammd Umar <umar.omtomo@gmail.com>
2020-08-13 14:00:26 +0500

    added tubo false and also resolve success page issue and added metas

--- app/javascript/packs/exclusive_ee_deals.js
+++ app/javascript/packs/exclusive_ee_deals.js
@@ -7,10 +7,10 @@ class ExclusiveEEDeals extends Common {
     this.getFormDetails('#dealsform');
 
     $('.btn-deal').click(function() {
-      debugger
       CI.postData()
       window.location = CI.details.success_url+"?device="+ $(this).prev('input').val()
     });
   }
+  successUrl(){}
 }
 export default new ExclusiveEEDeals();
