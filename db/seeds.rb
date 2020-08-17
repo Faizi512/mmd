da341564ec95cf5bcb92869cd03afbd77a1a035b
Muhammd Umar <umar.omtomo@gmail.com>
2020-08-17 23:20:23 +0500

    remove extra code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-require 'csv'
-CSV.foreach('o2_sheet.csv', headers: true ) do |row|
-  retailers = row['Retailers']
-  img = row['Url']
-  p_type = row['Type']
-  title = row['Title']
-  color = row['Colours']
-  monthly = row['Monthly'].to_i
-  upfront = row['Upfront'].to_i
-  data = row['Data'].to_i
-  call = row['Calls'].to_i
-  text = row['Text'].to_i
-  contr = row['Contract'].to_i
-  roaming = row['Roaming']
-  f_gift = row['Gifts']
-   
-  Product.create(retailer: retailers, img: img, product_type: p_type, title: title, color: color, monthly_cost: monthly, upfront: upfront, data: data, calls: call, text: text, contract: contr, free_gift: f_gift, data_roaming: roaming,) 
-end
-puts "done"
