ff712a03109c128d8906d6b196d31662aa8fddcd
Muhammd Umar <umar.omtomo@gmail.com>
2020-08-17 23:20:28 +0500

    remove extra code

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,6 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
   config.action_controller.allow_forgery_protection = false
-  config.hosts << "1946adce8ff2.ngrok.io"
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
