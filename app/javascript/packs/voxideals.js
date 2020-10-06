e8afd9ab28279728c0c4fa1f665bc58221127ffb
umar482 <umar.omtomo@gmail.com>
2020-10-06 16:00:51 +0500

    completed design form integration remain

---
+++ app/javascript/packs/voxideals.js
@@ -0,0 +1,59 @@
+import Common from "./common.js"
+class VoxiDeals extends Common {
+  constructor() {
+    super();
+    var CI = this;
+    this.validate("#dealform")
+    this.getFormDetails('#dealform')
+    this.showToolTip()
+    this.showClock()
+    this.fillform()
+    this.popupTerms()
+    this.showTab(this.currentTab);
+
+    $('.carousel').carousel({
+      interval: 2000
+    })
+    $( ".property" ).change(function() {
+      var tabs = $(".tab");
+      tabs[CI.currentTab].style.display = "none";
+      CI.currentTab = CI.currentTab + 1;
+      CI.showTab(CI.currentTab);
+      $('.towncity').val($(this).find("option:selected").data("city"))
+      $('.street1').val($(this).find("option:selected").data("street"))
+      $('.county').val($(this).find("option:selected").data("province"))
+      $('.houseNumber').val($(this).find("option:selected").data("housenum"))
+    });
+
+    window.Parsley.on('field:error', function() {
+      $(".btn-success").removeClass("in-progress")
+      $(".tab").removeClass("in-progress")
+    });
+
+    $( "#btn-continue" ).click(() => {
+      CI.nextStep(1)
+    });
+
+    $( "#btn-back" ).click(function() {
+      CI.backStep(-1)
+    });
+
+    $(document).on("click", '.open-form', function() {
+      CI.phoneName = $(this).find('input').val()
+      $('#deal-form-modal').modal('show')
+      $('.clock').hide()
+      event.preventDefault();
+    });
+  }
+  fixStepIndicator(num) {
+    var progress = document.getElementById('progressBar');
+    if(num >= 0) {
+      progress.style.width = (num*50)+"%";
+      progress.innerText = "Progress " + (num*50) + "%";
+      if( num ==  0){
+        progress.innerText = '';
+      }
+    }
+  }
+}
+export default new VoxiDeals();
