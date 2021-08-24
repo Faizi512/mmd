620d3c75110c8a3cd7a7b5e548df765472c9ce98
umar482 <umar.omtomo@gmail.com>
2021-08-24 10:48:56 +0500

    added campaign id 1073

--- app/controllers/lead_controller.rb
+++ app/controllers/lead_controller.rb
@@ -52,12 +52,12 @@ class LeadController < ApplicationController
     redirect_to params[:url]
   end
 
-  def lead_search
+  def lookup_lead_365(key, camp_id)
     data =  {
-      "key": "6c67a8ee57305998506ad0bc2c08e296",
+      "key": key,
       "searches": [
         {
-          "campaignId": 767,
+          "campaignId": camp_id,
           "phone": params[:phone]
         }
       ]
@@ -70,9 +70,16 @@ class LeadController < ApplicationController
     request.body = data.to_json
     response = http.request(request)
     puts "****" * 30
-    puts result = JSON.parse(response.body)
+    puts @result = JSON.parse(response.body)
+  end
+
+  def lead_search
+    lookup_lead_365(key = '6c67a8ee57305998506ad0bc2c08e296', camp_id = 767)
+    if @result["totalMatches"] == 0
+      lookup_lead_365(key = '3f2af16146e4c986136846e74f2640bf' , camp_id = 1073)
+    end
     puts "****" * 30
-    render json: {status: 200, match: result["totalMatches"]}
+    render json: {status: 200, match: @result["totalMatches"]}
   end
 
   def accept_leads
