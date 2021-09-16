b2bd2017b0b3b28546dd29da0b053ee9b5042702
umar482 <umar.omtomo@gmail.com>
2021-09-16 15:10:21 +0500

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -30,23 +30,6 @@ ActiveRecord::Schema.define(version: 2021_09_09_052424) do
     t.text "source"
   end
 
-  create_table "exit_leads", force: :cascade do |t|
-    t.string "name"
-    t.string "receiver"
-    t.string "url"
-    t.string "functional"
-    t.integer "priority"
-    t.integer "cap"
-    t.integer "total_count"
-    t.float "percentage"
-    t.float "copy_percentage"
-    t.string "operational", default: "active"
-    t.integer "count", default: 0
-    t.boolean "is_default", default: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "lead_counts", force: :cascade do |t|
     t.string "url"
     t.date "redirect_date"
