b150005283aa8389b6497bd848fef07fba0c1be1
umar482 <umar.omtomo@gmail.com>
2021-09-16 13:19:01 +0500

    code improvement

--- app/controllers/api/v1/redirect_controller.rb
+++ app/controllers/api/v1/redirect_controller.rb
@@ -4,9 +4,10 @@ class Api::V1::RedirectController < ApplicationController
     record = ExitDelivery.find(params[:id]).increment!(:count).increment!(:total_count)
     if record.cap && record.count >= record.cap
       record.update(operational: "disable")
-
-      exit_deliveies = ExitDelivery.active.where.not(id: record.id).where(status: record.status).filter{
-        |url| url if(url.source & record.source).present? }
+      exit_deliveies =
+        ExitDelivery.active.where.not(id: record.id).where(status: record.status).filter do |url|
+          url if(url.source & record.source).present?
+        end
       total_percent = exit_deliveies.sum(&:percentage)
       individual_percent = record.percentage.to_f / total_percent
 
@@ -29,7 +30,6 @@ class Api::V1::RedirectController < ApplicationController
 
     sold_redirect_urls = sold_exit_urls_list.compact.any? ? sold_exit_urls_list.compact : exit_urls.active_sold_with_no_source
     unsold_redirect_urls = unsold_exit_urls_list.compact.any? ? unsold_exit_urls_list.compact  : exit_urls.active_unsold_with_no_source
-
     @soldUrl = get_url(sold_redirect_urls, 'sold')
     @unsoldUrl = get_url(unsold_redirect_urls, 'unsold')
     render json:{sold_url: @soldUrl, unsold_url: @unsoldUrl}, status: :ok
