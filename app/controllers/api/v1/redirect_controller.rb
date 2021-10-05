6040242c2f7d4879a9cc4c523097c99b1c581a5d
umar482 <umar.omtomo@gmail.com>
2021-10-05 20:00:23 +0500

    fixed count bug

--- app/controllers/api/v1/redirect_controller.rb
+++ app/controllers/api/v1/redirect_controller.rb
@@ -23,10 +23,17 @@ class Api::V1::RedirectController < ApplicationController
   def exit_deliveries
     exit_urls = ExitDelivery.all
 
-    sold_exit_urls_list =
-      exit_urls.active_sold.map{|url| url if url.source.present? && url.source.include?(params[:source])}
-    unsold_exit_urls_list =
-      exit_urls.active_unsold.map{|s| s if s.source.present? && s.source.include?(params[:source])}
+    if params[:source].present?
+      sold_exit_urls_list =
+        exit_urls.active_sold.map{|url| url if url.source.present? && url.source.map(&:downcase).include?(params[:source].downcase)}
+      unsold_exit_urls_list =
+        exit_urls.active_unsold.map{|s| s if s.source.present? && s.source.map(&:downcase).include?(params[:source].downcase)}
+    else
+      sold_exit_urls_list =
+        exit_urls.active_sold.map{|url| url unless url.source.present? }.compact
+      unsold_exit_urls_list =
+        exit_urls.active_unsold.map{|s| s unless s.source.present? }.compact
+    end
 
     sold_redirect_urls = sold_exit_urls_list.compact.any? ? sold_exit_urls_list.compact : exit_urls.active_sold_with_no_source
     unsold_redirect_urls = unsold_exit_urls_list.compact.any? ? unsold_exit_urls_list.compact  : exit_urls.active_unsold_with_no_source
