de73397bebe5b2996143e03ffcdc2b89e7a59e53
umar482 <umar.omtomo@gmail.com>
2022-01-06 16:38:20 +0500

    updated migration

--- db/migrate/20211124070926_add_is_mobile_in_exit_deliveries.rb
+++ db/migrate/20211124070926_add_is_mobile_in_exit_deliveries.rb
@@ -1,5 +1,8 @@
 class AddIsMobileInExitDeliveries < ActiveRecord::Migration[6.0]
   def change
+    remove_column :exit_deliveries, :is_mobile, :boolean, default: false
+    remove_column :exit_deliveries, :source_rule, :integer, :default => 0
+
     add_column :exit_deliveries, :is_mobile, :boolean, default: false
     add_column :exit_deliveries, :source_rule, :integer, :default => 0
   end
