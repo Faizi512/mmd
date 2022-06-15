126c20a7ad70b289a80e2e5b194e12ce43d85a09
Faizan Haider <hfaizan53@gmail.com>
2022-06-15 21:01:14 +0000

    new partner added in checkbox list

--- app/javascript/packs/policies.js
+++ app/javascript/packs/policies.js
@@ -3,21 +3,25 @@ class Policies {
     this.getPolicies();
     $( document ).ajaxSuccess(function() {
       var chk1 = $("input[type='checkbox'][name='agree']");
-      if(chk1.length === 0) {
-        var check = $("input[type='checkbox'][name='partners-tp']");
-        check.prop('checked',true)
-      }
+
     });
   }
 
   getPolicies(){
     $('.content').each(function(){
-      let element = $(this)
+      var element = $(this)
       $.ajax({
         type: "GET",
         url: `https://mobilematcher.co.uk/policies/${element.attr('data-page')}?brand=deals.megamobiledeals.com`,
         success: function(res) {
           element.html(res.html)
+          if(element.attr('data-page') == "tooltip"){
+            $('<div class="col-md-6"><div class="custom-control custom-checkbox mr-sm-2"><input type="checkbox" checked name="partners-tp" class="custom-control-input" id="debtReportToday" data-parsley-multiple="partners-tp"><label class="custom-control-label text-left text-secondary label-size" for="debtReportToday">Debt Report Today</label></div></div>').appendTo($(".partner-form").children()[0])
+            let inputs  = document.querySelectorAll('.custom-control-input')
+            for(let i in inputs){
+              inputs[i].checked = true
+            } 
+          }        
         },
         dataType: "json"
       })
